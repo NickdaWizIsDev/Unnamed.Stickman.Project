7f37562692d65b1cf5cf0d6a88a0ed51efd145b7
NickdaWizIsDev <nicocastrito2005@gmail.com>
2023-08-28 08:26:45 -0400

    0.2
    No longer a stickman project, animations done for the 3d model.

---
+++ Assets/Scripts/Global Scripts/AnimStrings.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using UnityEngine;
+
+public class AnimStrings : MonoBehaviour
+{
+    internal static string isMoving = "isMoving";
+    internal static string isGrounded = "isGrounded";
+    internal static string canMove = "canMove";
+    internal static string jumpTrig = "jump";
+    internal static string atkTrig = "atk";
+    internal static string yVel = "yVel";
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,7 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -7,43 +10,138 @@ public class PlayerMovement : MonoBehaviour
     public float fallSpeed = -10f;
     public float gravity = -9.81f;
 
+    public float CurrentMoveSpeed
+    {
+        get
+        {
+            if (CanMove)
+            {
+                return IsMoving ? moveSpeed : 0;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        set
+        {
+
+        }
+    }
+
+    public bool isGrounded;
+
+    private bool isFacingRight = true;
+    public bool IsFacingRight
+    {
+        get
+        {
+            return isFacingRight;
+        }
+        private set
+        {
+            if (isFacingRight != value)
+            {
+                // Calculate the new rotation based on facing direction
+                Vector3 newScale = transform.localScale ;
+                newScale.x *= -1f;
+
+                // Apply the new rotation to the character's transform
+                transform.localScale = newScale;
+            }
+
+            isFacingRight = value;
+        }
+    }
+
+    private bool isMoving;
+    public bool IsMoving
+    {
+        get
+        {
+            return isMoving;
+        }
+
+        private set
+        {
+            isMoving = value;
+            anim.SetBool(AnimStrings.isMoving, value);
+        }
+    }
+    public bool CanMove
+    {
+        get
+        {
+            return anim.GetBool(AnimStrings.canMove);
+        }
+    }
+
+    [SerializeField] private Vector2 movement;
+    [SerializeField] private Vector3 velocity;
+
     private CharacterController controller;
-    [SerializeField]private Vector3 velocity;
+    private Animator anim;
 
     private void Awake()
     {
         controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();        
     }
 
     private void Update()
     {
         // Check if the player is grounded
-        bool isGrounded = controller.isGrounded;
+        isGrounded = controller.isGrounded;
 
         // Gravity application
         if (isGrounded && velocity.y < 0)
         {
-            velocity.y = -2f;
+            velocity.y = -5f;
         }
-        else if (!isGrounded && velocity.y < 0)
+        else if (!isGrounded)
         {
-            velocity.y = Mathf.Lerp(velocity.y, fallSpeed, 2f * Time.deltaTime);
+            velocity.y = Mathf.Lerp(velocity.y, fallSpeed, 1.5f * Time.deltaTime);
         }
 
-        // Movement input and application
-        float moveInput = Input.GetAxis("Horizontal");
-        Vector3 moveDirection = transform.right * moveInput;
-        controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+        anim.SetFloat(AnimStrings.yVel, velocity.y);
+        anim.SetBool(AnimStrings.isGrounded, isGrounded);
 
-        // Jump input and application
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            // Calculate the initial jump velocity based on jumpForce and gravity
-            velocity.y = Mathf.Sqrt(-2f * jumpForce * gravity);
-        }
+        Vector3 moveDirection = transform.right * movement;
+        controller.Move(moveSpeed * Time.deltaTime * moveDirection);
 
         // Apply gravity
-        velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+        IsMoving = movement != Vector2.zero;
+
+        SetFacingDirection(movement);
+    }
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            if (isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(-2f * jumpForce * gravity);
+                anim.SetTrigger(AnimStrings.jumpTrig);
+                Debug.Log("Jump!");
+            }
+        }
     }
 }
